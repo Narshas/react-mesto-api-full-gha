61315149a3093194bc91387257b38c106420f758
Mark Zaitsev <mark.zaytsev@yandex.ru>
2023-07-28 22:19:38 +0200

    fixing pv4 error

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(requestLogger);
 app.use(cors);
 app.use(helmet());
 app.use(cookieParser());
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/mestodb', { family: 4 });
 
 app.use(router);
 app.use(errorLogger);
